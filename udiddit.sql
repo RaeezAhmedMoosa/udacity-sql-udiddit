6d926b63fd676deb494cab64ab3f75f9d8eace21
Raeez <raeez.burner@gmail.com>
2021-04-28 11:57:02 +0200

    feat: Add DDL code for Part II Guideline 1(b)

--- udiddit.sql
+++ udiddit.sql
@@ -95,7 +95,7 @@ separate tables for the following entities:
 
 
 /*
-According to Guideline 1(s), the "users" table must have the following rules:
+According to Guideline 1(a), the "users" table must have the following rules:
 
 1.1 usernames must be unique
 1.2 usernames have a maximum length of 25 characters
@@ -133,3 +133,76 @@ INSERT INTO "users" ("username")
 -- This an insertion of a username that exceeds the maximum character length
 INSERT INTO "users" ("username")
   VALUES ('012345678901234567890123456789');
+
+
+
+/*
+According to Guideline 1(b), the "topics" table must have the following rules:
+
+2.1 topic names must be unique
+2.2 topic name has a maximum character length of 30 characters
+2.3 topic name can't be empty
+2.4 topics can have an optional description of a maximum of 500 characters
+*/
+
+-- This works as intended for DDL purposes
+CREATE TABLE "topics" (
+  "id" SERIAL,
+  "name" VARCHAR(30) NOT NULL,
+  "description" TEXT,
+  CONSTRAINT "topics_pk" PRIMARY KEY ("id"),
+  CONSTRAINT "unique_topic_names" UNIQUE ("name"),
+  CONSTRAINT "no_empty_topic_names"
+  CHECK (LENGTH(TRIM("name")) > 0),
+  CONSTRAINT "description_limit"
+  CHECK (LENGTH("description") <= 500)
+);
+
+/*
+Below are the test DML queries that were used to test and verify that the Udiddit
+business rules were working.
+*/
+
+-- This is a simple test DML adding a topic with no description
+INSERT INTO "topics" ("name")
+  VALUES ('newcastleunited');
+
+/*
+This is a test DML which adds a topic and a description. It worked properly when
+executed, but the description output didn't look right, as the formatting was
+exactly identical to how it was inputted in the DML
+
+Also, I may have found a solution to the grammatical inconsistency regarding the
+case-sensitivity of the topic names in the original data set. I am using the
+LOWER function to ensure that any upper case characters are converted into lower
+case characters when being inserted into the new "topics" table.
+
+Another note, the ESCAPE command for SQL when you need to include a single
+quotation mark within a string is "'" (the single quotation mark). So for this
+DML it cannot be ('...Sony's...') or ('...Sony\'s...'). The correct syntax is
+('...Sony''s...'). You basically need to use 2 single quotation marks.
+*/
+INSERT INTO "topics" ("name", "description")
+  VALUES (LOWER('PSP'), 'A subdiddit for all things Playstation Portable, the red-headed and oft-forgotten handheld console from our overlords at Sony. Hacked too soon, and now there''s no store available');
+
+
+-- This is a simple test DML adding a topic with a short description
+INSERT INTO "topics" ("name", "description")
+  VALUES ('psvita', 'A subdiddit for all things PS Vita');
+
+
+-- This is a test DML which attempts to add a blank topic
+INSERT INTO "topics" ("name")
+  VALUES ('');
+
+
+  -- This is another test DML which attempts to add a blank topic
+  INSERT INTO "topics" ("name")
+    VALUES ('                           ');
+
+
+-- This a test DML adding a topic and a long description
+INSERT INTO "topics" ("name", "description")
+  VALUES ('asoiaf', 'This is a subdiddit for fans of the Song of Ice and Fire series. While GRR Martin is taking his sweet, sweet time finishing the "Winds of Winter", we can all hang out here and wonder how more than a decade has passed since the last proper ASOIAF book.');
+
+  
