377f07013c9bf1c26d3d0ec9957b7a60b222d389
Raeez <raeez.burner@gmail.com>
2021-04-30 15:32:52 +0200

    feat: Add DQL code for Guideline 2(b) concerning users

--- udiddit_dql.sql
+++ udiddit_dql.sql
@@ -16,3 +16,42 @@ SELECT "id",
        "login_date"
 FROM "users"
 WHERE "login_date" <= '2020-04-30'; -- sample date, please change accordingly
+
+
+
+/*
+Guideline 2(b) – List all users who haven’t created any posts
+*/
+
+-- Test DQL which returns the post count per user id
+SELECT "user_id",
+       COUNT("id")
+FROM "posts"
+GROUP BY 1
+ORDER BY 2;
+
+
+-- Test DQL which returns the post count per username
+SELECT "u"."username",
+       COUNT("p"."id") AS post_count
+FROM "users" "u"
+LEFT JOIN "posts" "p"
+ON "u"."id" = "p"."user_id"
+GROUP BY 1
+ORDER BY 2;
+
+
+-- DQL which filters out the data to focus on users with 0 posts
+WITH user_post_count AS (
+  SELECT "u"."username" AS username,
+         COUNT("p"."id") AS post_count
+  FROM "users" "u"
+  LEFT JOIN "posts" "p"
+  ON "u"."id" = "p"."user_id"
+  GROUP BY 1
+)
+SELECT "username",
+       "post_count"
+FROM "user_post_count"
+WHERE "post_count" = 0
+ORDER BY 1; -- Arranges the usernames in alphabetical order
