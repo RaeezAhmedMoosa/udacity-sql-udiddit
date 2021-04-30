0fa52bc57e8c1f391379cf1094b689f2c1c97b08
Raeez <raeez.burner@gmail.com>
2021-04-30 15:02:13 +0200

    feat: Add DDL & DQL for Guideline 2(a) concerning users

--- udiddit_dql.sql
+++ udiddit_dql.sql
@@ -1,3 +1,18 @@
 ### udiddit
 
 ### Part II: udiddit Web Analytics
+
+/*
+Guideline 2(a) – List all users who haven’t logged in during the last year
+*/
+
+-- Test DDL
+ALTER TABLE "users"
+  ADD COLUMN "login_date" TIMESTAMP;
+
+-- Test DQL
+SELECT "id",
+       "username",
+       "login_date"
+FROM "users"
+WHERE "login_date" <= '2020-04-30'; -- sample date, please change accordingly
